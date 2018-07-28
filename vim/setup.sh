eae8d0af7d2165c153585432d981570499ca2cdd
inkrel <santiago.cardona@inkaviation.com>
2018-07-27 23:12:27 -0700

    vim settings: setup script

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,2 +1,15 @@
-mkdir ~/.config
-ln -s ~/dotfiles/nvim/ ~/.config/
+#!/bin/sh
+
+# nvim settings folders/files
+[ -f ~/.config ] || mkdir ~/.config
+[ -f ~/.config/nvim ] || ln -s ~/dotfiles/vim ~/.config/nvim
+
+# install vim-plug for nvim
+[ -f ~/.local/share/nvim/site/autoload/plug.vim ] || curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# vim settings folders/files
+[ -f ~/.vim ] || mkdir ~/.vim
+[ -f ~/.vimrc ] || ln -s ~/dotfiles/vim/.vimrc ~/
+
+# install vim-plug for vim
+[ -f ~/.vim/autoload/plug.vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
