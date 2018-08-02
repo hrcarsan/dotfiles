b27e744b1e57c90369b73ed8e6b59fa563efc4f7
Santiago Herrera Cardona <santiago.cardona@inkaviation.com>
2018-08-01 22:21:39 -0500

    Update install_nvim.sh

--- vim/install_nvim.sh
+++ vim/install_nvim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#sudo apt-get install -y software-properties-common
-#sudo add-apt-repository ppa:neovim-ppa/stable
-#sudo apt-get update
-#sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip
 
 sudo -H pip2 install --upgrade pip
 sudo -H pip3 install --upgrade pip
