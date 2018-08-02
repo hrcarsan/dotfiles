c9c6b985ed91096e66248f352b26e5df021effee
inkrel <santiago.cardona@inkaviation.com>
2018-08-01 21:05:57 -0700

    update vim/setup.sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # nvim settings folders/files
-[ -f ~/.config ] || mkdir ~/.config
-[ -f ~/.config/nvim ] || ln -s ~/dotfiles/vim ~/.config/nvim
+[ -d ~/.config ] || mkdir ~/.config
+[ -d ~/.config/nvim ] || ln -s ~/dotfiles/vim ~/.config/nvim
 
 # install vim-plug for nvim
 [ -f ~/.local/share/nvim/site/autoload/plug.vim ] || curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # vim settings folders/files
-[ -f ~/.vim ] || mkdir ~/.vim
+[ -d ~/.vim ] || ln -s ~/dotfiles/vim  ~/.vim
 [ -f ~/.vimrc ] || ln -s ~/dotfiles/vim/.vimrc ~/
 
 # install vim-plug for vim
-[ -f ~/.vim/autoload/plug.vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ -f ~/.vim/autoload/plug.vim ] || curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim +PlugInstall +qall
+
+echo "Finish!"
