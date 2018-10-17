e4d936ecf469432057eb64bc5029e7b4751362c1
Santiago Herrera Cardona <santiagohecar@gmail.com>
2018-10-17 00:00:34 -0500

    vim configs: updates

--- vim/config/auto_save.vim
+++ vim/config/auto_save.vim
@@ -1,2 +1,10 @@
 let g:auto_save = 1
 let g:auto_save_silent = 1
+
+let g:auto_save_presave_hook = 'call AbortAutoSave()'
+
+function! AbortAutoSave()
+  if &filetype == 'go'
+    let g:auto_save_abort = 1
+  endif
+endfunction
--- vim/install_nvim.sh
+++ vim/install_nvim.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip
+if [ "$(id -u)" != "0" ]; then
+   echo "Run with sudo"
+   exit 1
+fi
+
+# Install dependecies
+apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+
+cd /usr/local/src
+export NVIM_VERSION=0.3.1
+wget https://github.com/neovim/neovim/archive/v$NVIM_VERSION.tar.gz
+tar -xzvf v$NVIM_VERSION.tar.gz
+cd neovim-$NVIM_VERSION/
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/usr/local/neovim-$NVIM_VERSION -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+make install
+rm /usr/bin/nvim
+ln -s /usr/local/neovim-$NVIM_VERSION/bin/nvim /usr/bin/nvim
+cd ..
+rm -r v$NVIM_VERSION.tar.gz neovim-$NVIM_VERSION
+
+# configure python
+apt-get install -y python-dev python-pip python3-dev python3-pip
 
 sudo -H pip2 install --upgrade pip
 sudo -H pip3 install --upgrade pip
